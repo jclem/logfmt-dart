55e3bf6a625c364e12fd2cd146334c43440df4c8
Jonathan Clem <jonathan@jclem.net>
2014-09-04 00:21:27 -0400

    Add doc comment to streamDecoder

--- lib/stream_decoder.dart
+++ lib/stream_decoder.dart
@@ -1,5 +1,9 @@
 part of logfmt;
 
+/**
+ * Returns a [StreamTransformer] which [decode]s logfmt-style [String]s and
+ * emits [Map]s.
+ */
 StreamTransformer streamDecoder() {
   return new StreamTransformer.fromHandlers(
     handleData: (String line, EventSink<Map<String, dynamic>> sink) {
